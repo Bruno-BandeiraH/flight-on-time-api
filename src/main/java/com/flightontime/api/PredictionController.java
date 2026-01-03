65e3883f6ebf6e6b7af4aaf795a5f172b5b810f0
Rafael Vieira <rafael.cordeiro011@hotmail.com>
2026-01-03 15:30:44 -0300

    feat: adicionando anotação valid no controller predict

--- src/main/java/com/flightontime/api/PredictionController.java
+++ src/main/java/com/flightontime/api/PredictionController.java
@@ -3,6 +3,7 @@ package com.flightontime.api;
 import com.flightontime.api.dto.RequestDTO;
 import com.flightontime.api.dto.ResponseDTO;
 import com.flightontime.api.service.PredictionService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +19,7 @@ public class PredictionController {
     }
 
     @PostMapping("/predict")
-    public ResponseEntity<ResponseDTO> getPrediction(@RequestBody RequestDTO data) {
+    public ResponseEntity<ResponseDTO> getPrediction(@RequestBody @Valid RequestDTO data) {
         ResponseDTO response = predictionService.predict(data);
         return ResponseEntity.ok(response);
     }
