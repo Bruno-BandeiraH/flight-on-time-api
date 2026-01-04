e3d8f9e61fb0330826396a7b12833f8a5cbf6021
Rafael Vieira <rafael.cordeiro011@hotmail.com>
2026-01-04 18:16:33 -0300

    feat: adicionando exception global para alguns erros e para erros especificos 400, 500, json

--- src/main/java/com/flightontime/api/infra/exception/RestExceptionHandler.java
+++ src/main/java/com/flightontime/api/infra/exception/RestExceptionHandler.java
@@ -1,18 +1,42 @@
-package com.flightontime.api.infra;
+package com.flightontime.api.infra.exception;
 
+import com.flightontime.api.infra.ValidatorException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
 
+    // Global handler
     @ExceptionHandler(ValidatorException.class)
-    public ResponseEntity<String> handleValidationExceptions(ValidatorException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+    public ResponseEntity<ValidationError> handleValidationExceptions(ValidatorException ex) {
+        var error = new ValidationError(ex.getField(), ex.getMessage());
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> error404(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> error500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("ERRO: Ocorreu um erro inesperado");
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ValidationError> jsonError(HttpMessageNotReadableException ex) {
+        var erro = new ValidationError("json", "O corpo da requisição está malformado ou com erro de sintaxe");
+        return ResponseEntity.badRequest().body(erro);
+    }
+
+    private record ValidationError(String field, String message) {
+        public ValidationError(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
