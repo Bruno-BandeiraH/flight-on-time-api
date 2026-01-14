9717007d6cc0c13a16607a04e1e884c79d97a4d7
Bruno <brunohenrique0665@gmail.com>
2026-01-14 17:27:02 -0300

    feat: adding Dockerfile

--- src/main/java/com/flightontime/api/config/WebClientConfig.java
+++ src/main/java/com/flightontime/api/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.flightontime.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${predictor.api.url}")
+    private String baseUrl;
+
     @Bean
     public WebClient predictionWebClient() {
         return WebClient.builder()
-            .baseUrl("http://localhost:8000")
+            .baseUrl(baseUrl)
             .build();
     }
 }
